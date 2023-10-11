8df8778101d41c9a65863996506bf6bf7a638929
Anna Gryazeva <anna_gryazeva@mail.ru>
2023-10-11 16:58:18 +0300

    new commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,7 @@
 public class BmiService {
     public int calculate (int weightInKg, double hightInMeters) {
+        double bmi = weightInKg / (hightInMeters * hightInMeters);
+        return (int) bmi;
 
     }
 }
